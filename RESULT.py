934a2a6f59855d99e52ea74406f9d26738ea30a3
ASI-CASPER <90950981+ASI-CASPER@users.noreply.github.com>
2021-09-20 15:11:09 +0530

    Add files via upload

---
+++ RESULT.py
@@ -0,0 +1,12 @@
+x = int(input("enter your marks :-"))
+if (x >= 75) :
+    print("'A'pass") 
+elif(x >= 65) :
+    print("'B' pass")
+elif(x >= 55) :
+    print("'C'pass")
+elif(x >= 35) :
+    print("'S'pass")
+else :
+    print("fail") 
+        
